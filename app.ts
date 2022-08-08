37d012885a3027151e265f7e2d26fb1472aaca83
Nurudeen Ahmed <110522344+EminentNewtonian@users.noreply.github.com>
2022-08-08 12:47:38 +0100

    index

---
+++ app.ts
@@ -0,0 +1,8 @@
+//require the express module
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+http.listen(port, () => {
+  console.log("Running on Port: " + port);
+});
